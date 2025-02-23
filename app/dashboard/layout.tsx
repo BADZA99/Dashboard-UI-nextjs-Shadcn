46ae49cd1d02b1bbbeef2f11b46c5fd848d9d15f
papabn <pndiaye999@gmail.com>
2025-02-23 00:01:33 +0000

    Mobile menu (part 2) using useMediaQuery hook from shadcn repo

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,29 @@
+'use client';
 import React from 'react'
 import MainMenu from './components/MainMenu';
 import MenuTitle from './components/menu-title';
+import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
+import { MenuIcon } from 'lucide-react';
+import { useMediaQuery } from '@/hooks/use-media-query';
 
 export default function DashboardLayout({children}:{children:React.ReactNode}) {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   return (
     <div className="grid md:grid-cols-[250px_1fr] h-screen">
          {/* dashboard */}
 <MainMenu classname='hidden md:flex' />
    {/* mobile menu */}
-      <div className="p-4 block md:hiddensticky top-0 left-0 bg-background border-b border-border">
+     { !isDesktop && (<div className="p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border">
         <MenuTitle />
-      </div>
+        <Drawer>
+          <DrawerTrigger>
+            <MenuIcon/>
+          </DrawerTrigger>
+          <DrawerContent>
+            <MainMenu classname='flex' />
+          </DrawerContent>
+        </Drawer>
+      </div>)}
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome Back , PAPABN</h1>
         {children}
