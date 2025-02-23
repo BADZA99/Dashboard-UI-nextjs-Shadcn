38c08ee5f3db6232af2d1795e97b5178d8e903ff
papabn <pndiaye999@gmail.com>
2025-02-23 13:22:51 +0000

    Create the loading skeleton for the employees table

--- app/dashboard/players/page.tsx
+++ app/dashboard/players/page.tsx
@@ -1,3 +1,49 @@
-export default function PlayersPage() {
-  return <div>players</div>;
+export default async function PlayersPage() {
+  // Fonction pour simuler un délai
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  // Attendre 5 secondes
+  await delay(5000);
+
+  const players = [
+    {
+      id: 1,
+      firstName: "John",
+      lastName: "Doe",
+      teamName: "Team A",
+      isTeamLeader: true,
+      avatar: "/images/cm.jpg",
+    },
+    {
+      id: 2,
+      firstName: "Jane",
+      lastName: "Smith",
+      teamName: "Team B",
+      isTeamLeader: false,
+      avatar: "/images/cm.jpg",
+    },
+    {
+      id: 3,
+      firstName: "Alice",
+      lastName: "Johnson",
+      teamName: "Team A",
+      isTeamLeader: false,
+      avatar: "/images/cm.jpg",
+    },
+    {
+      id: 4,
+      firstName: "Bob",
+      lastName: "Brown",
+      teamName: "Team C",
+      isTeamLeader: true,
+      avatar: "/images/cm.jpg",
+    },
+  ];
+
+  return (
+    <div>
+     
+    </div>
+  );
 }
